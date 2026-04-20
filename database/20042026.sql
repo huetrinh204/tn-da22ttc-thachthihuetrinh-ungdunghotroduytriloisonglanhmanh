9f7f7ae64a94468f4cd236abd53e5369a173277b
huetrinh204 <trinhfokko@gmail.com>
2026-04-20 16:33:06 +0700

    Add database

--- database/20042026.sql
+++ database/20042026.sql
@@ -53,15 +53,18 @@ DROP TABLE IF EXISTS `habit_logs`;
 CREATE TABLE `habit_logs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `habit_id` int NOT NULL,
-  `date` date NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
+  `log_date` date NOT NULL,
   `value` int DEFAULT '0',
   `is_completed` tinyint(1) DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `note` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `habit_id` (`habit_id`,`date`),
+  UNIQUE KEY `habit_id` (`habit_id`,`log_date`),
+  UNIQUE KEY `unique_log` (`habit_id`,`log_date`),
   KEY `idx_logs_habit` (`habit_id`),
   CONSTRAINT `habit_logs_ibfk_1` FOREIGN KEY (`habit_id`) REFERENCES `habits` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +73,7 @@ CREATE TABLE `habit_logs` (
 
 LOCK TABLES `habit_logs` WRITE;
 /*!40000 ALTER TABLE `habit_logs` DISABLE KEYS */;
+INSERT INTO `habit_logs` VALUES (6,2,7,'2026-04-20',0,0,'2026-04-20 09:28:57',NULL),(7,1,7,'2026-04-20',0,0,'2026-04-20 09:28:57',NULL),(8,3,7,'2026-04-20',0,0,'2026-04-20 09:29:15',NULL);
 /*!40000 ALTER TABLE `habit_logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,17 +87,21 @@ DROP TABLE IF EXISTS `habits`;
 CREATE TABLE `habits` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` enum('eat','sleep','exercise','mental') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `target_value` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` enum('eat','exercise','sleep','mental','hydration','other') COLLATE utf8mb4_unicode_ci DEFAULT 'other',
+  `target_value` int DEFAULT '1',
   `unit` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `reminder_time` time DEFAULT NULL,
   `is_active` tinyint(1) DEFAULT '1',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `icon` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT '⭐',
+  `color` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT '#4CAF50',
+  `frequency` enum('daily','weekly') COLLATE utf8mb4_unicode_ci DEFAULT 'daily',
+  `target_count` int DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `idx_habits_user` (`user_id`),
   CONSTRAINT `habits_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,6 +110,7 @@ CREATE TABLE `habits` (
 
 LOCK TABLES `habits` WRITE;
 /*!40000 ALTER TABLE `habits` DISABLE KEYS */;
+INSERT INTO `habits` VALUES (1,7,'aa','other',1,NULL,NULL,1,'2026-04-20 09:24:10','⭐','#4CAF50','daily',1),(2,7,'b','eat',1,NULL,NULL,1,'2026-04-20 09:24:33','?','#4CAF50','daily',1),(3,7,'c','other',1,NULL,NULL,1,'2026-04-20 09:29:10','?','#4CAF50','daily',1);
 /*!40000 ALTER TABLE `habits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -185,7 +194,7 @@ CREATE TABLE `streaks` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `streaks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -194,6 +203,7 @@ CREATE TABLE `streaks` (
 
 LOCK TABLES `streaks` WRITE;
 /*!40000 ALTER TABLE `streaks` DISABLE KEYS */;
+INSERT INTO `streaks` VALUES (1,7,1,1,'2026-04-20');
 /*!40000 ALTER TABLE `streaks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -222,7 +232,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `idx_users_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -231,7 +241,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Test User','test@gmail.com','123456',NULL,'user',1,1,'2026-04-17 03:59:58',NULL,NULL,NULL,NULL,NULL),(2,'Trinh','trinh@gmail.com','123456',NULL,'user',1,1,'2026-04-17 05:14:38',NULL,NULL,NULL,NULL,NULL),(4,'testpostman','testpostman@gmail.com','$2b$10$SMgcdMWOHOs7AuWfTR5/0ucJVkRD6LrfdnItDvscdQGjhmL18I94O',NULL,'user',1,1,'2026-04-17 05:24:07',NULL,NULL,NULL,NULL,NULL),(5,'testlogin','testlogin@gmail.com','$2b$10$804Utew7BvJ8nhmAUVUnGufoFQF9kVa3mSdbh1F./7Ut915xtDqpu',NULL,'user',1,1,'2026-04-17 05:52:34',NULL,NULL,NULL,NULL,NULL),(6,'Huệ Trinh','trinhfokko@gmail.com','',NULL,'user',1,1,'2026-04-20 07:46:15',NULL,NULL,NULL,NULL,NULL),(7,'Meo Meo','meomeodthvch@gmail.com','',NULL,'user',1,1,'2026-04-20 08:01:11',NULL,NULL,NULL,NULL,NULL),(8,'testdk','testdk@gmail.com','$2b$10$UW5OHcJ9F91hfl2p9jdGcuHr.G2MIbjDs3DiodSNp2wdhClIOU.gy',NULL,'user',1,1,'2026-04-20 08:22:25',NULL,NULL,NULL,NULL,NULL),(9,'test','test@gmail.','$2b$10$2v/e7uLLkmA1UvdO3.lLsebWJfyRgD5h2595PDHk3WF.w61Q88m8.',NULL,'user',1,1,'2026-04-20 08:39:51',NULL,NULL,NULL,NULL,NULL),(10,'test','testne@gmail.com','$2b$10$zwvNW8lz9CEyAPSyOCSoyuepXBffHStfkFlTFr1KyAoENh3r/Ak4a',NULL,'user',1,1,'2026-04-20 08:45:10','other',2019,157.00,56.00,'[\"eat_healthy\"]');
+INSERT INTO `users` VALUES (1,'Test User','test@gmail.com','123456',NULL,'user',1,1,'2026-04-17 03:59:58',NULL,NULL,NULL,NULL,NULL),(2,'Trinh','trinh@gmail.com','123456',NULL,'user',1,1,'2026-04-17 05:14:38',NULL,NULL,NULL,NULL,NULL),(4,'testpostman','testpostman@gmail.com','$2b$10$SMgcdMWOHOs7AuWfTR5/0ucJVkRD6LrfdnItDvscdQGjhmL18I94O',NULL,'user',1,1,'2026-04-17 05:24:07',NULL,NULL,NULL,NULL,NULL),(5,'testlogin','testlogin@gmail.com','$2b$10$804Utew7BvJ8nhmAUVUnGufoFQF9kVa3mSdbh1F./7Ut915xtDqpu',NULL,'user',1,1,'2026-04-17 05:52:34',NULL,NULL,NULL,NULL,NULL),(6,'Huệ Trinh','trinhfokko@gmail.com','',NULL,'user',1,1,'2026-04-20 07:46:15',NULL,NULL,NULL,NULL,NULL),(7,'Meo Meo','meomeodthvch@gmail.com','',NULL,'user',1,1,'2026-04-20 08:01:11',NULL,NULL,NULL,NULL,NULL),(8,'testdk','testdk@gmail.com','$2b$10$UW5OHcJ9F91hfl2p9jdGcuHr.G2MIbjDs3DiodSNp2wdhClIOU.gy',NULL,'user',1,1,'2026-04-20 08:22:25',NULL,NULL,NULL,NULL,NULL),(9,'test','test@gmail.','$2b$10$2v/e7uLLkmA1UvdO3.lLsebWJfyRgD5h2595PDHk3WF.w61Q88m8.',NULL,'user',1,1,'2026-04-20 08:39:51',NULL,NULL,NULL,NULL,NULL),(10,'test','testne@gmail.com','$2b$10$zwvNW8lz9CEyAPSyOCSoyuepXBffHStfkFlTFr1KyAoENh3r/Ak4a',NULL,'user',1,1,'2026-04-20 08:45:10','other',2019,157.00,56.00,'[\"eat_healthy\"]'),(11,'demo','demo@gmail.com','$2b$10$WthRKl06zz4OMoT1UAKqIeL9wLcE1luJ3UHTZovLQ0k7Kh2GNftzO',NULL,'user',1,1,'2026-04-20 08:59:20','female',2004,157.00,47.00,'[\"hydration\", \"other:Tang can\"]');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -244,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-20 15:47:29
+-- Dump completed on 2026-04-20 16:32:48
